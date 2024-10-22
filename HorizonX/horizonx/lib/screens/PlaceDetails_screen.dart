9128705b8b211c79ca241fd1848f5efaa8c4381a
Badr MOhamed <badrmohamedragab2003@gmail.com>
2024-10-22 03:04:41 +0300

    solve search screen error

--- HorizonX/horizonx/lib/screens/PlaceDetails_screen.dart
+++ HorizonX/horizonx/lib/screens/PlaceDetails_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:horizonx/Model/Restaurant.dart';
 import 'package:horizonx/constants.dart';
 import 'package:horizonx/models/place.dart';
 class PlaceDetailsScreen extends StatelessWidget {
