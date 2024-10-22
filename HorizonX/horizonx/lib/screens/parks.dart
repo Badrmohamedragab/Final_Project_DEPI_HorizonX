6d2ca1ad3b34f8eb0d8470cf4b7376d91ba14592
Badr MOhamed <badrmohamedragab2003@gmail.com>
2024-10-22 20:42:27 +0300

    refactor the code

--- HorizonX/horizonx/lib/screens/parks.dart
+++ HorizonX/horizonx/lib/screens/parks.dart
@@ -4,6 +4,7 @@ import '../data.dart';
 import '../models/place.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/place_card.dart';
+import '../widgets/place_view.dart';
 
 class Parks extends StatelessWidget {
   Parks({super.key});
@@ -11,12 +12,6 @@ class Parks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: custom_app_bar(context, "Parks"),
-    body: ListView.builder(
-    itemCount: parksList.length,
-    itemBuilder: (context, _) =>
-    (cafesOrRestaurants(context, parksList[_]))),
-    );
+    return placeView(context, "Parks", parksList);
   }
 }
--- HorizonX/horizonx/lib/screens/restaurants.dart
+++ HorizonX/horizonx/lib/screens/restaurants.dart
@@ -3,6 +3,7 @@ import '../constants.dart';
 import '../models/restaurant.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/place_card.dart';
+import '../widgets/place_view.dart';
 
 class Restaurants extends StatelessWidget {
   List<Restaurant> restaurantList = [];
@@ -11,52 +12,6 @@ class Restaurants extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    restaurantList = [
-      Restaurant(
-          cityId: "Alexandria",
-          name: "Adrenaline Restaurant",
-          description: "American restaurant",
-          location: "https://maps.app.goo.gl/Uvgnr4Y6E8iBgx6F6",
-          address:
-              "Beside Olympic Club Gate and Hambaka cafe، 11 Ahmed Zewail Square, Bab Sharqi WA Wabour Al Meyah, Bab Sharqi, Alexandria Governorate 21500",
-          longitude: "29.918188",
-          latitude: "31.200063",
-          picture:
-              "https://lh5.googleusercontent.com/p/AF1QipOaJXKbd7IQ6uiC5VUQvf9PUHcm5V6_4o8sNjcq",
-          rate: 5.0,
-          numberOfRatings: 3),
-      Restaurant(
-          cityId: "Alexandria",
-          name: "Ruby's cafe and Restaurant",
-          description: "American restaurant",
-          location: "https://maps.app.goo.gl/egZevZzbaoZZC7xL7",
-          address:
-              "3 Kafr Abdou, Abu an Nawatir, Sidi Gaber, Alexandria Governorate 21523",
-          longitude: "29.957437",
-          latitude: "31.223312",
-          picture:
-              "https://lh5.googleusercontent.com/p/AF1QipP0DmKWcu9iGZ40yiU7iTMePyI4N0gyBjz04s8h",
-          rate: 4.6,
-          numberOfRatings: 152),
-      Restaurant(
-          cityId: "Cairo",
-          name: "Egyptian Nights",
-          description: "Egyptian restaurant",
-          location: "https://maps.app.goo.gl/vWmWAEMsL6w6Sg5Y8",
-          address:
-              "Cairo Marriott Hotel - 16 Saray El, Gezira St, Zamalek, Cairo Governorate 11211",
-          longitude: "31.224312",
-          latitude: "30.056812",
-          picture:
-              "https://lh5.googleusercontent.com/p/AF1QipNr-YuDf_RbXdpOtC1QkqpTu375PHtceX-82sGC",
-          rate: 4.4,
-          numberOfRatings: 322)
-    ];
-    return Scaffold(
-      appBar: custom_app_bar(context, "Restaurants"),
-      body: ListView.builder(
-          itemCount: restaurantList.length,
-          itemBuilder: (context, _) => (cafesOrRestaurants(context , restaurantList[_]))),
-    );
+    return placeView(context, "Restaurants", restaurantList);
   }
 }
